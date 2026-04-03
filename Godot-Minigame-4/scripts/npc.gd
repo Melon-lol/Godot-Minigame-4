c255532e1f7b43e3a1199df1fc082992b59797e0
Soo Lee <sool3626@gmail.com>
2026-04-03 12:33:27 -0700

    ballin

--- Godot-Minigame-4/scripts/npc.gd
+++ Godot-Minigame-4/scripts/npc.gd
@@ -1,10 +1,10 @@
 extends RigidBody3D
 
 # step 12: make these all tune-able in the Inspector
-var throwPower: float = 1.0
-var speed: float = 1.0
-var maxDistance: float = 2.0
-var throwFrequency: float = 3.0
+@export var throwPower: float = 1.0
+@export var speed: float = 1.0
+@export var maxDistance: float = 2.0
+@export var throwFrequency: float = 3.0
 
 @onready var ballScene = preload("res://scenes/ball.tscn")
 
@@ -14,18 +14,18 @@ var distance: float = 0.0
 var xDir: float = 1.0
 
 # step 13: add a time variale
-
+var _timer
 
 
 # step 4: uncomment the function below
-"""
+
 func _ready() -> void:
 	contact_monitor = true
 	max_contacts_reported = 3
 	
 	ball = get_node("Ball")
 	ball._pickup()
-"""
+
 
 
 func _physics_process(delta: float) -> void:
@@ -39,7 +39,11 @@ func _physics_process(delta: float) -> void:
 		
 	# step 14: use the delta variable to keep a timer
 	# when the timer runs out, have the NPC throw a ball
+	_timer+= delta
 	
+	if _timer > throwFrequency:
+		_throw()
+		_timer = 0
 
 func _throw_ball() -> void:
 	if ball == null:
@@ -62,11 +66,13 @@ func _throw_ball() -> void:
 
 
 # step 15: uncomment the function below
-"""
+
 func _on_body_shape_entered(_body_rid: RID, body: Node, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body is Ball:
 		_hitByBall()
-"""
+
 
 # step 16: create the _hitByBall() function below here
-	
+func _hitByBall() -> void:
+	points+= 1
+	%PointsLabel.text = str(points)
